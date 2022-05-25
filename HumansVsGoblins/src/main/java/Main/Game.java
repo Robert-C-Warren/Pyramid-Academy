6f9e8b4bf5ab239f74d4610e59fba7d27d703c22
Robert Warren <bobbywarren14@gmail.com>
2022-05-25 13:34:29 -0700

    Added main game thread and FPS/UPS counter. Added comments throughout and refactored methods.

--- HumansVsGoblins/src/main/java/Main/Game.java
+++ HumansVsGoblins/src/main/java/Main/Game.java
@@ -1,5 +1,7 @@
 package Main;
 
+import org.w3c.dom.ls.LSOutput;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -7,23 +9,25 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Game extends JFrame {
-
+public class Game extends JFrame implements Runnable {
     private GameScreen gameScreen;
-
     private BufferedImage img;
+    private Thread gameThread;
+    private final double FPS_SET = 60.0;
+    private final double UPS_SET = 60.0;
+
     public Game() {
-
+        // Import Sprites
         importImg();
-
+        // Set Size Of Game Screen
         setSize(640,640);
-        setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         gameScreen = new GameScreen(img);
         add(gameScreen);
+        setVisible(true);
     }
-
+    // Import Images For Game
     private void importImg() {
         InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
 
@@ -31,6 +35,50 @@ public class Game extends JFrame {
             img = ImageIO.read(is);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    // Main Game Thread
+    void start(){
+        gameThread = new Thread(this) {};
+
+        gameThread.start();
+    }
+
+
+    @Override
+    // Game
+    public void run() {
+        // Doubles to calculate FPS & UPS
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
+        // To record and display FPS & UPS
+        int frames = 0;
+        int updates = 0;
+
+        long lastFrame = System.nanoTime();
+        long lastTimeCheck = System.currentTimeMillis();
+        long lastUpdate = System.nanoTime();
+        // Game Loop
+        while (true) {
+        // Render
+            if (System.nanoTime() - lastFrame >= timePerFrame) {
+                repaint();
+                lastFrame = System.nanoTime();
+                frames++;
+            }
+
+        // Update
+            if (System.nanoTime() - lastUpdate >= timePerUpdate) {
+                lastUpdate = System.nanoTime();
+                updates++;
+            }
+
+            if (System.currentTimeMillis() - lastTimeCheck >= 1000) {
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
+                frames = 0;
+                updates = 0;
+                lastTimeCheck = System.currentTimeMillis();
+            }
         }
     }
 }
--- HumansVsGoblins/src/main/java/Main/GameScreen.java
+++ HumansVsGoblins/src/main/java/Main/GameScreen.java
@@ -9,41 +9,38 @@ import java.util.Random;
 public class GameScreen extends JPanel {
     private Random random;
     private BufferedImage img;
-    private ArrayList<BufferedImage> sprites = new ArrayList<>();
 
-    public GameScreen(BufferedImage img) {
+    private ArrayList<BufferedImage> sprites = new ArrayList<>(); // Add Sprites To Array List For Easier Access
+
+    public GameScreen(BufferedImage img) { // Loads Random Sprites
         this.img = img;
 
         loadSprites();
 
         random = new Random();
+
     }
 
-    private void loadSprites() {
+    private void loadSprites() { // Nested Loops To Fill The Entire Game Screen
 
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 sprites.add(img.getSubimage(j * 32, i * 32, 32, 32));
             }
         }
-
     }
 
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) { // Paints The Screen With Random Sprites
         super.paintComponent(g);
-
-//        g.drawImage(sprites.get(8), 0, 0, null);
-
 
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 20; j++) {
                 g.drawImage(sprites.get(getRndInt()), j * 32, i * 32, null);
             }
         }
-
     }
 
-    private int getRndInt() {
+    private int getRndInt() { // Generates Random Index For Array List
         return random.nextInt(100);
     }
 }
