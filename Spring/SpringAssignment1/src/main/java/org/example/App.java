9c7dafa2dea53e8ea04e8c22b4a50f93ecb50123
Robert Warren <bobbywarren14@gmail.com>
2022-06-21 11:01:05 -0700

    Component

--- Spring/SpringAssignment1/src/main/java/org/example/App.java
+++ Spring/SpringAssignment1/src/main/java/org/example/App.java
@@ -1,13 +1,13 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("StudentSpring.xml");
+        Student obj = (Student) context.getBean("student");
+        System.out.println(obj);
     }
 }
